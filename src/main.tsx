45b77fa035ad7d53ef32e37b1089f3350b435767
Jicheng Zhi <jzhi0004@student.monash.edu>
2023-08-13 15:11:34 +1000

    Implement page routing with [React Router](https://reactrouter.com/en/main)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './style.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Root from "./routes/Root.tsx";
+import ErrorPage from "./routes/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                index: true,
+                element: <h1>Home Page</h1>
+            },
+            {
+                path: "about-us",
+                element: <h1>About Us Page</h1>,
+            },
+            {
+                path: "projects",
+                element: <h1>Projects Page</h1>
+            },
+            {
+                path: "teams",
+                element: <h1>Teams Page</h1>
+            },
+            {
+                path: "recruitment",
+                element: <h1>Recruitment Page</h1>
+            },
+            {
+                path: "resources",
+                element: <h1>Resources Page</h1>
+            },
+            {
+                path: "more",
+                element: <h1>More Page</h1>
+            },
+        ]
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
+);
