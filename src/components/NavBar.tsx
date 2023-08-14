996ce502af2abf4b60b29309719e0162becada50
Jicheng Zhi <jzhi0004@student.monash.edu>
2023-08-14 15:48:42 +1000

    Refactor conditional tailwind classes with `twJoin`

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import {Link, NavLink} from "react-router-dom";
 import {useState} from "react";
-import classNames from "classnames";
 import {FiMenu, FiMoon, FiSun} from "react-icons/fi";
+import {twJoin} from "tailwind-merge";
 
 type Route = {
     name: string
@@ -39,35 +39,34 @@ const routes: Route[] = [
     },
 ];
 
-function Menu({closeMenu}) {
+function Menu({closeMenu}: {closeMenu: () => void}) {
     return (
         <div className='h-full max-h-screen overflow-y-scroll no-bg-scrollbar bg-white dark:bg-[#23272f]'>
             <div className='py-10 h-full w-full flex flex-col gap-3'>
                 {routes.map(route => (
-                    <Link to={route.href} key={route.name}
-                          className='w-full hover:bg-gray-200 dark:hover:bg-gray-700 py-2'
-                          onClick={closeMenu}>
+                    <NavLink to={route.href} key={route.name}
+                             className={({isActive}) => twJoin('w-full py-2',
+                                 isActive
+                                     ? 'text-[#006DAE] dark:text-[#006DAE] bg-gray-200 dark:bg-gray-700'
+                                     : 'text-black dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700')}
+                             onClick={closeMenu}>
                         <span className='font-medium px-10 dark:text-white'>{route.name}</span>
-                    </Link>
+                    </NavLink>
                 ))}
             </div>
         </div>
     );
 }
 
-function MonashAutomationLogo({mode}: { mode: string }) {
-
+function MonashAutomationLogo({theme, className = ''}: { theme: 'dark' | 'light', className: string }) {
+    const url = theme === 'dark' ? '/ma-logo-lettermark-white-blue.png' : '/ma-logo-lettermark-black-blue.png';
     return (
-        mode === 'dark'
-            ? <img src='/ma-logo-lettermark-white-blue.png' alt='Monash Automation Logo'
-                   className='h-8 w-8'/>
-            : <img src='/ma-logo-lettermark-black-blue.png' alt='Monash Automation Logo'
-                   className='h-8 w-8'/>
-    )
+        <img src={url} alt='Monash Automation Logo' className={className}/>
+    );
 }
 
 // TODO: gray background #f3f4f6 monash blue #006DAE
-export default function NavBar({theme, toggleTheme}) {
+export default function NavBar({theme, toggleTheme}: {theme: 'dark' | 'light', toggleTheme: () => void}) {
 
     const [showMenu, setShowMenu] = useState(false);
 
@@ -75,29 +74,28 @@ export default function NavBar({theme, toggleTheme}) {
         return (
             <button
                 className='w-12 h-12 flex justify-center items-center rounded-full active:scale-95 hover:bg-gray-200 dark:hover:bg-gray-700'
-                onClick={() => toggleTheme()}
-            >
+                onClick={() => toggleTheme()}>
                 {theme === 'light' ? <FiMoon/> : <FiSun className='stroke-white'/>}
             </button>
         );
     }
 
+
     return (
-        <div className={classNames('sticky top-0 bg-gray-50', {
-            'z-20': !showMenu,
-            'h-screen z-50': showMenu
-        })}>
+        <div className={twJoin('sticky top-0 bg-gray-50',
+            showMenu ? 'h-screen z-50' : 'z-20'
+        )}>
             <nav className='flex flex-row justify-between items-center py-2 px-5 dark:bg-[#23272f]'>
 
                 <div className='flex flex-row justify-center items-center gap-5'>
                     <button
                         className='lg:hidden flex justify-center items-center w-12 h-12 rounded-full active:scale-95 hover:bg-gray-200 dark:hover:bg-gray-700'
-                        onClick={() => setShowMenu(x => !x)}>
+                        onClick={() => setShowMenu(!showMenu)}>
                         <FiMenu className='dark:stroke-white'/>
                     </button>
 
                     <Link to='/' className='flex flex-row justify-center items-center gap-5'>
-                        <MonashAutomationLogo mode={theme}/>
+                        <MonashAutomationLogo theme={theme} className='h-8 w-8'/>
                         <span className='font-medium dark:text-white'>Monash Automation</span>
                     </Link>
 
@@ -105,10 +103,11 @@ export default function NavBar({theme, toggleTheme}) {
 
                 <div className='hidden lg:flex flex-row gap-5'>
                     {routes.map(route => (
-                        <NavLink to={route.href} key={route.name} className={({isActive}) => {
-                            if (isActive) return 'text-[#006DAE] dark:text-[#006DAE] bg-gray-200 dark:bg-gray-700 py-2 px-3 rounded-3xl font-medium';
-                            else return 'dark:text-white dark:hover:bg-gray-700 py-2 px-3 rounded-3xl';
-                        }}>
+                        <NavLink to={route.href} key={route.name}
+                                 className={({isActive}) => twJoin('py-2 px-3 rounded-3xl font-medium',
+                                     isActive
+                                         ? 'text-[#006DAE] dark:text-[#006DAE] bg-gray-200 dark:bg-gray-700'
+                                         : 'dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700')}>
                             <span>{route.name}</span>
                         </NavLink>
                     ))}
